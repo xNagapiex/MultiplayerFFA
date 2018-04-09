6e9d7bf062b81a6f5fe452e9a9325668c44d4543
KRonja <ronjakukko@kamk.fi>
2018-04-09 15:11:06 +0300

    plugin

---
+++ Server/Database/ClassLibrary1/ClassLibrary1/Tags.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+static class Tags
+{
+    public static readonly ushort SpawnPlayerTag = 0;
+    public static readonly ushort MovePlayerTag = 1;
+}
